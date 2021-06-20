d7f421ddfc7973e59c6dbff69d6ad9a211fb062c
geraud simon <geraud.simon99@gmail.com>
2021-06-20 15:42:28 +0200

    first commit geraud

--- webapp-skypattern/src/store/modules/common/common.actions.js
+++
@@ -1,7 +0,0 @@
-import ApiSkyppattern from '@/services/http/api-skypattern'
-
-export default {
-  async getListStatus({ commit }) {
-    commit('setListStatus', await ApiSkyppattern.get(`/analysis/status`))
-  },
-}
--- webapp-skypattern/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transpileDependencies: ['vuetify'],
-  devServer: {
-    disableHostCheck: true,
-    port: 8080,
-    public: '0.0.0.0:8080',
-  },
-  publicPath: '/',
-}
